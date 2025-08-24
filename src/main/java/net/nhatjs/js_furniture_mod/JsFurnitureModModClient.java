874da9836f82e3449a36ffa0a45cb2db57474b04
NhatJS <nhatjsouthfall7@gmail.com>
2025-08-24 23:50:22 +0700

    added important comment

---
+++ src/main/java/net/nhatjs/js_furniture_mod/JsFurnitureModModClient.java
@@ -0,0 +1,10 @@
+package net.nhatjs.js_furniture_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JsFurnitureModModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
