c3cbcf5afbad032b4496971b22f457f3a149d444
NhatJS <nhatjsouthfall7@gmail.com>
2025-10-09 21:33:46 +0700

    update to 1.21.6-1.21.8

--- src/main/java/net/nhatjs/js_furniture_mod/entity/client/ChairBlockEntity.java
+++ src/main/java/net/nhatjs/js_furniture_mod/entity/client/ChairBlockEntity.java
@@ -4,13 +4,13 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.data.DataTracker;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.storage.ReadView;
+import net.minecraft.storage.WriteView;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ChairBlockEntity extends Entity {
-
     public ChairBlockEntity(EntityType<?> type, World world) {
         super(type, world);
     }
@@ -21,14 +21,20 @@ public class ChairBlockEntity extends Entity {
     }
 
     @Override
-    protected void readCustomDataFromNbt(NbtCompound nbt) {
+    public boolean damage(ServerWorld world, DamageSource source, float amount) {
+        return false;
+    }
+
+    @Override
+    protected void readCustomData(ReadView view) {
 
     }
 
     @Override
-    protected void writeCustomDataToNbt(NbtCompound nbt) {
+    protected void writeCustomData(WriteView view) {
 
     }
+
 
     @Override
     protected void removePassenger(Entity passenger) {
@@ -36,11 +42,6 @@ public class ChairBlockEntity extends Entity {
         if(!this.getWorld().isClient()) {
             this.kill(((ServerWorld) this.getWorld()));
         }
-    }
-
-    @Override
-    public boolean damage(ServerWorld world, DamageSource source, float amount) {
-        return false;
     }
 
     @Override
