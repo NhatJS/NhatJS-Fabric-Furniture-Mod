977e42cdde87e7ba2c95e4ee20255c8ae3b84955
NhatJS <nhatjsouthfall7@gmail.com>
2025-11-22 10:36:56 +0700

    fix player sit that crash game

--- src/main/java/net/nhatjs/js_furniture_mod/entity/client/renderer/SeatRenderer.java
+++ src/main/java/net/nhatjs/js_furniture_mod/entity/client/renderer/SeatRenderer.java
@@ -18,6 +18,6 @@ public class SeatRenderer extends EntityRenderer<SeatBlockEntity, EntityRenderSt
 
     @Override
     public EntityRenderState createRenderState() {
-        return null;
+        return new EntityRenderState();
     }
 }
